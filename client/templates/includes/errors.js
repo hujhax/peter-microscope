74e8efa1df7ec5659eccc4f859904cea68bc8905
peter-rogers <peter@peter.com>
2014-11-17 15:16:10 -0600

    Using our new error-reporting functionality.

--- client/templates/includes/errors.js
+++ client/templates/includes/errors.js
@@ -3,3 +3,12 @@ Template.errors.helpers({
         return Errors.find();
     }
 });
+
+// remove each error a few seconds after you render it.
+Template.error.rendered = function() {
+    var error = this.data;
+    Meteor.setTimeout(function() {
+        Errors.remove(error._id);
+    }, 3000);
+};
+a
