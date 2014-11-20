5e1121449db103388e100f01a74bd09800ccd31d
peter-rogers <peter@peter.com>
2014-11-20 16:13:03 -0600

    adding unit tests to errors package

--- packages/hujhax:errors/package.js
+++ packages/hujhax:errors/package.js
@@ -12,3 +12,9 @@ Package.onUse(function(api) {
         api.export('Errors');
 });
 
+Package.onTest(function(api) {
+    api.use('hujhax:errors', 'client');
+    api.use(['tinytest', 'test-helpers'], 'client');
+
+    api.addFiles('errors_tests.js', 'client');
+});
